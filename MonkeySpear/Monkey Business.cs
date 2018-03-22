0009c915ac27dc6c3457807f156703cb1181df60
NewPCJan-PC\New PC Jan <jrapeldoorn@hotmail.com>
2018-03-22 11:01:32 +0100

    Added the randomizer

--- MonkeySpear/Monkey Business.cs
+++ MonkeySpear/Monkey Business.cs
@@ -13,6 +13,7 @@ namespace MonkeySpear
     public partial class Form1 : Form
     {
         static Random _random = new Random();
+        private static string output;
 
         public Form1()
         {
@@ -21,7 +22,7 @@ namespace MonkeySpear
 
         private void start_btn_Click(object sender, EventArgs e)
         {
-
+            
         }
 
         public static char GetLetter()
@@ -29,6 +30,15 @@ namespace MonkeySpear
             int number = _random.Next(0, 26);
             char letter = (char)('a' + number);
             return letter;
+        }
+
+        public static string Randomizer()
+        {
+            while (output.Length < 100)
+            {
+                output = output + GetLetter();
+            }
+            return output;
         }
 
     }
