1b7ff44b01cc384cc3c9e6f7d4b80485c4c3c7ba
Bueno Montoya, Henry <610463@student.inholland.nl>
2018-03-22 10:42:34 +0100

    added UI

--- MonkeyBusinessLibrary/MonkeyBusinessLibrary.cs
+++ MonkeyBusinessLibrary/MonkeyBusinessLibrary.cs
@@ -9,10 +9,8 @@ namespace MonkeyBusinessLibrary
 {
     public class MonkeyBusinessLibrary
     {
-
         public void GetWords(List<string> words, string path)
         {
-
             using (StreamReader reader = new StreamReader(path))
             {
                 string[] filelines = File.ReadAllLines(path);
