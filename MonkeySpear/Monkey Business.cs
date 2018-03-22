9f5b9004df4df968e1f35a858a1884c1ac4f7133
Muradin, Randy <560105@student.inholland.nl>
2018-03-22 11:00:16 +0100

    Put random method within UI instead of Form.

--- MonkeySpear/Monkey Business.cs
+++ MonkeySpear/Monkey Business.cs
@@ -12,7 +12,6 @@ namespace MonkeySpear
 {
     public partial class Form1 : Form
     {
-        static Random _random = new Random();
 
         public Form1()
         {
@@ -23,13 +22,5 @@ namespace MonkeySpear
         {
 
         }
-
-        public static char GetLetter()
-        {
-            int number = _random.Next(0, 26);
-            char letter = (char)('a' + number);
-            return letter;
-        }
-
     }
 }
--- MonkeySpear/MonkeyBusinessUI.cs
+++ MonkeySpear/MonkeyBusinessUI.cs
@@ -8,6 +8,13 @@ namespace MonkeySpear
 {
     public static class MonkeyBusinessUI
     {
-        
+        static Random _random = new Random();
+
+        public static char GetLetter()
+        {
+            int number = _random.Next(0, 26);
+            char letter = (char)('a' + number);
+            return letter;
+        }
     }
 }
