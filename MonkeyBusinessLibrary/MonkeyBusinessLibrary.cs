89887450c658ac7ac24a4346b182e3b40f35ca14
Bueno Montoya, Henry <610463@student.inholland.nl>
2018-03-22 10:33:52 +0100

    Library added

---
+++ MonkeyBusinessLibrary/MonkeyBusinessLibrary.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace MonkeyBusinessLibrary
+{
+    public class MonkeyBusinessLibrary
+    {
+
+        public void GetWords(List<string> words, string path)
+        {
+
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string[] filelines = File.ReadAllLines(path);
+
+                foreach (var line in filelines)
+                {
+                    words.Add(line);
+                }
+            }
+           
+        }
+    }
+}
